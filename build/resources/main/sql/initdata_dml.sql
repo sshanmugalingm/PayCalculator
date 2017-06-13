d2ae11db190e62501259bbdd9d969607c85c5004
Senthur Shanmugalingm <sshanmugalingm@bglcorp.com.au>
2017-06-13 22:00:35 +1000

    Fixes

---
+++ build/resources/main/sql/initdata_dml.sql
@@ -0,0 +1,7 @@
+-- Tax Table Data
+
+insert into IncomeTaxChart (id, incomeStart, incomeEnd, taxableAmount, taxUnitRate, taxableThreshold) values (1, 0, 18200, 0, 0.0, 0.0);
+insert into IncomeTaxChart (id, incomeStart, incomeEnd, taxableAmount, taxUnitRate, taxableThreshold) values (2, 18201, 37000, 0, 0.19, 18200.00);
+insert into IncomeTaxChart (id, incomeStart, incomeEnd, taxableAmount, taxUnitRate, taxableThreshold) values (3, 37001, 80000, 3572, 0.325, 37000.00);
+insert into IncomeTaxChart (id, incomeStart, incomeEnd, taxableAmount, taxUnitRate, taxableThreshold) values (4, 80001, 180000, 17547, 0.37, 80000.00);
+insert into IncomeTaxChart (id, incomeStart, incomeEnd, taxableAmount, taxUnitRate, taxableThreshold) values (5, 180001, 99999999999, 54547, 0.45, 180000.00);
